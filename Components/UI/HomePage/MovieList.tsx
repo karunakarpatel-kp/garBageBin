ee5e853cfa90141ada67057093927bcaaeb6b94a
KarunakarPatel <karunakarpatel.kp@gmail.com>
2025-07-22 12:05:18 +0530

    Fixed ui for the home and telugu movies pages in lapTop view

--- Components/UI/HomePage/MovieList.tsx
+++ Components/UI/HomePage/MovieList.tsx
@@ -17,18 +17,18 @@ const MovieList = () => {
         </div>
       </div>
 
-      <div className="LoadingUI grid  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-11  mt-0 mb-8 mx-2 ">
+      <div className="LoadingUI grid  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-11 mt-0 mb-8 mx-2 border-0 border-white">
         {blogPostsObj.map((singleBlogPost, index) => {
           return (
             <div
               key={index}
-              className=" shadow-sm md:shadow-sm mt-0 m-auto p-0 md:p-0 no-underline dark:bg-slate-900 hover:underline hover:underline-offset-4 h-full relative"
+              className=" shadow-sm md:shadow-sm mt-0 m-auto p-0 md:p-0 no-underline dark:bg-slate-900 hover:underline hover:underline-offset-4 h-full relative border-0 border-green-800 "
             >
               <Link href={singleBlogPost.url} className="no-underline ">
                 <Image
                   src={singleBlogPost.featuredImage}
                   alt={singleBlogPost.featuredImageAltText}
-                  className="mt-3 mb-3  h-[350px] ring-1 rounded-md bg-slate-800 dark:bg-slate-800 m-auto border-0 max-w-xs lg:w-auto"
+                  className="mt-3 mb-3  h-[350px] ring-1 rounded-md bg-slate-800 dark:bg-slate-800 m-auto border-0 max-w-[98%] lg:w-auto"
                 />
                 <div className="z-50 text-center -mt-12 ">
                   <p className=" text-[#fffe08]  inline-block p-0 px-4 text-center m-auto bg-slate-800 rounded-md shadow-xl ">
